9833ee5eaf62d7cbd7b81f7bc32a1edbf1cb02fd
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-29 00:21:40 +0530

    starting endpoints

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("welcome to http server");
+});
+
+app.post("/signup", (req, res) => {
+  const { username, email, password } = req.body;
+  res.send(`signup success ${username} ${email} ${password}`);
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  res.send(`signin success ${email} ${password}`);
+});
+
+app.post("/createroom", (req, res) => {
+  res.send(`roomid is ${uuidv4()}`);
+});
+
+app.post("/joinroom", (req, res) => {
+  const { roomid } = req.body;
+  res.send(`joined room ${roomid}`);
 });
 
 app.listen(5050);
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,12 +1,13 @@
-import { WebSocketServer } from "ws";
+import WebSocket from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const ws = new WebSocket("ws://www.host.com/path");
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+ws.on("error", console.error);
 
-  ws.on("message", function message(data) {
-    // console.log("received: %s", data);
-    ws.send("something");
-  });
+ws.on("open", function open() {
+  ws.send("something");
+});
+
+ws.on("message", function message(data) {
+  console.log("received: %s", data);
 });
